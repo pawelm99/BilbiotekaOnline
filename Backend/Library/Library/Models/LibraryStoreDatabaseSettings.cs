8e8eb1e3c2ee0b75a25ea05ab07def634fda7baf
Paweł Martosz <pawel.m476@gmail.com>
2022-06-22 19:21:02 +0200

    Create LibraryStoreDatabaseSettings.cs

---
+++ Backend/Library/Library/Models/LibraryStoreDatabaseSettings.cs
@@ -0,0 +1,11 @@
+﻿using WebApi.Interface;
+
+namespace WebApi.Models
+{
+    public class LibraryStoreDatabaseSettings : ILibraryStoreDatabaseSettings
+    {
+        public string BooksCollectionName { get; set; }
+        public string ConnectionString { get; set; }
+        public string DatabaseName { get; set; }
+    }
+}
