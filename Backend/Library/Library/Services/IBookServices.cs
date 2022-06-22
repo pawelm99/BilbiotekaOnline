1c432c9096db85ce5b55df17cc85e73480b01337
Paweł Martosz <pawel.m476@gmail.com>
2022-06-22 19:20:33 +0200

    Create IBookServices.cs

---
+++ Backend/Library/Library/Services/IBookServices.cs
@@ -0,0 +1,14 @@
+﻿using WebApi.Models;
+
+namespace WebApi.Services
+{
+    public interface IBookServices
+    {
+        List<Book> GetBooks();
+        Book GetBookById(string id);
+        Book Create(Book book);
+        void Update(string id, Book book);
+        void Delete(string id);
+
+    }
+}
