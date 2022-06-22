2f304e811055b9067d86b83c85563e6b171c306d
Paweł Martosz <pawel.m476@gmail.com>
2022-06-22 19:20:59 +0200

    Create Book.cs

---
+++ Backend/Library/Library/Models/Book.cs
@@ -0,0 +1,16 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace WebApi.Models
+{
+    [BsonIgnoreExtraElements]
+    public class Book
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string Id { get; set; } 
+
+        [BsonElement("name")]
+        public string Name { get; set; } 
+    }
+}
