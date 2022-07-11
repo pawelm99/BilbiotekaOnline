73169ace221c4f876081e1fb3010a969ea522ccb
Paweł Martosz <pawel.m476@gmail.com>
2022-07-11 18:18:30 +0200

    Zmiany w App.js

--- Frontend/library/src/App.js
+++ Frontend/library/src/App.js
@@ -3,15 +3,22 @@ import React from 'react';
 import './App.css';
 
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
-import NavbarApp from './NavbarApp';
+import NavbarApp from './components/NavbarApp';
+
+import Books from './components/pages/Books';
+
 function App() {
   return (
     <>
       <Router>
         <NavbarApp></NavbarApp>
-        
+        <Routes>
+        <Route path="/Books" element={<Books  />} />
+        </Routes>
       </Router>
     </>
+
+    
   );
 }
 
