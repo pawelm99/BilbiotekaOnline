d43be0d669b4fd68bbaef455b9df3a9ac866f23b
Paweł Martosz <pawel.m476@gmail.com>
2022-07-12 18:03:55 +0200

    Create BookPost.cs

---
+++ Backend/Library/Library/Models/BookPost.cs
@@ -0,0 +1,18 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace WebApi.Models
+{
+    public class BookPost
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string Id { get; set; }
+
+        [BsonElement("name")]
+        public string Name { get; set; }
+
+        [BsonElement("image")]
+        public string ImageString { get; set; }
+    }
+}
